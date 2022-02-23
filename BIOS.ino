39473bae0f1bf5e4bc183b56163c8854099f5cba
iamsfw <91392681+iamsfw@users.noreply.github.com>
2022-02-22 19:33:20 -0800

    Add files via upload
    Keyboard setup for ATMega32U4 Dev Boards to make a virtual keyboard that types on Dell BIOS passwords, and disables it automatically.

---
+++ BIOS.ino
@@ -0,0 +1,50 @@
+#include <Keyboard.h>
+ 
+void setup() {
+  Keyboard.begin();
+  delay(1000);
+  Keyboard.print("8@tM@n");
+  Keyboard.press(KEY_RETURN);
+  delay(10);
+  Keyboard.releaseAll();
+  delay(1400);
+  Keyboard.press(KEY_LEFT_ARROW);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_DOWN_ARROW);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_DOWN_ARROW);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_DOWN_ARROW);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_RIGHT_ARROW);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_DOWN_ARROW);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_TAB);
+  delay(10);
+  Keyboard.releaseAll();
+  delay(600);
+  Keyboard.print("8@tM@n");
+  Keyboard.press(KEY_TAB);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_TAB);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_TAB);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.press(KEY_RETURN);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.end();
+  
+}
+   
+void loop() {}
